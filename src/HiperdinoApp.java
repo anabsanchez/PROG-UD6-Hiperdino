3b3aa704ca5e1c192d09bbc227e2113232fe19dc
anabsanchez <usr2015@salesianos-lacuesta.net>
2024-04-29 21:45:01 +0100

    feat: Customer-products interaction mechanism

---
+++ src/HiperdinoApp.java
@@ -0,0 +1,7 @@
+public class HiperdinoApp {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
---
+++ src/com/hiperdino/elements/Product.java
@@ -0,0 +1,16 @@
+package com.hiperdino.elements;
+
+public class Product {
+    
+    private String name;
+
+    public Product(String name) {
+
+        this.name = name;
+    }
+
+    public String toString() {
+
+        return this.name;
+    }
+}
